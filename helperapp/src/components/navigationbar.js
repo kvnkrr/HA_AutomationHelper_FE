3feda7a3b1b98efffe4c2e6dfe46d695ffac6cdf
kvnkrr <121253179+kvnkrr@users.noreply.github.com>
2023-02-26 19:47:33 +0000

    added ookla into navbar

--- helperapp/src/components/navigationbar.js
+++ helperapp/src/components/navigationbar.js
@@ -10,6 +10,7 @@ export const Navigationbar= () =>{
            <Nav>
               <NavLink eventKey="1" as={Link} to="/"> Home </NavLink>
               <NavLink eventKey="2" as={Link} to="/refuge"> Refuge </NavLink>
+              <NavLink eventKey="3" as={Link} to="/ookla"> Ookla </NavLink>
             </Nav>
           </Navbar.Collapse> 
         </Navbar> 
